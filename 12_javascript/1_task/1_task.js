c7e431a3b1925b92a5afe68f412291fd2b8dda48
venera13 <lobanova_venera@mail.ru>
2017-08-23 20:24:15 +0300

    Сделала задание по JavaScript Объекты

--- 12_javascript/1_task/1_task.js
+++ 12_javascript/1_task/1_task.js
@@ -1,23 +1,27 @@
 var userMessage = prompt("Введите сообщение:");
 var keyWords = ["зима", "Весна", "лето", "осень"];
+
+function creatStars(word)
+{
+    var stars = "*";
+    for (var k = 1; k < word.length; k++)
+    {
+        stars += "*";
+    }
+    return stars;
+}
+
 function moderateMessage()
 {
-    for (var i = 0; i < userMessage.length; i++)
+    var message = userMessage.toLowerCase();
+    for (var j = 0; j < keyWords.length; j++)
     {
-        var message = userMessage.toLowerCase();
-        for (var j = 0; j < keyWords.length; j++)
+        var keyWord = keyWords[j].toLowerCase();
+        var stars = creatStars(keyWord);
+        if (message.indexOf(keyWord) !== -1)
         {
-            var keyWord = keyWords[j].toLowerCase();
-            var stars = "*";
-            for (var k = 1; k < keyWord.length; k++)
-            {
-                stars += "*";
-            }
-            if (message.indexOf(keyWord) !== -1)
-            {
-                userMessage = userMessage.replace(keyWord, stars);
-                break;
-            }
+            userMessage = userMessage.replace(keyWord, stars);
+            break;
         }
     }
     console.log(userMessage);
